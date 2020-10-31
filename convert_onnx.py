de62651476326de1e1c691c9f064116b04b82dc8
kafan1986 <debasish1986@gmail.com>
2020-10-31 07:34:26 +0530

    Create convert_onnx.py

---
+++ convert_onnx.py
@@ -0,0 +1,18 @@
+import torchreid
+
+torchreid.models.show_avai_models()
+
+model = torchreid.models.build_model(name='osnet_x0_25', num_classes=1041)
+torchreid.utils.load_pretrained_weights(model, "osnet_x0_25.pth") 
+
+model.eval()
+
+from torch.autograd import Variable
+import torch
+import onnx
+
+# An example input you would normally provide to your model's forward() method.
+input = torch.ones(1, 3, 96, 48)
+raw_output = model(input)
+
+torch.onnx.export(model, input, 'osnet_ain_x1_0.onnx', verbose=False, export_params=True)
